f994d6cf4c23c13aaa9fa506d5cc8b3a4cddc8ef
MaJhinn-Buu <isaiahthomas.villasper.cics@ust.edu.ph>
2023-12-13 08:37:37 +0800

    Waddup My Backend Master

--- app/src/main/java/com/example/groupactivity/MainMenu_Admin.kt
+++ app/src/main/java/com/example/groupactivity/MainMenu_Admin.kt
@@ -1,6 +1,5 @@
 package com.example.groupactivity
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,29 +8,24 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.groupactivity.R.id.USERbackbutton
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-
-class MainMenu_User : AppCompatActivity() {
-//This is For Main Menu Spinner and Buttons
+class MainMenu_Admin : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var dataList: ArrayList<DataClass>
     lateinit var imageList: Array<Int>
     lateinit var titleList: Array<String>
 
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainmenu_userlayout)
-        var backBtn :ImageButton = findViewById(R.id.USERbackbutton)
+        setContentView(R.layout.mainmenu_adminlayout)
+        var backBtn :ImageButton = findViewById(R.id.ADMINbackbutton)
 
         //We'll Replace The Values Below Using The Database Value For Logo
         imageList= arrayOf(R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background)
         titleList = arrayOf("Numero Uno", "Numero Dos", "Numero Tres", "Numero Quatro")
 
-        recyclerView = findViewById(R.id.MMLeaderboard_User)
+        recyclerView = findViewById(R.id.MMLeaderboard_Admin)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(false)
         dataList = arrayListOf<DataClass>()
