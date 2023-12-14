4fd5af12eafd624c8a623e564b45925cc51ac77f
Dylan <curtdylan.adalem.cics@ust.edu.ph>
2023-12-14 21:11:23 +0800

    working race result data recording

--- app/src/main/java/com/example/groupactivity/AddRaceResultPage.kt
+++ app/src/main/java/com/example/groupactivity/AddRaceResultPage.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AddRaceResultPage : AppCompatActivity() {
 
@@ -21,6 +26,7 @@ class AddRaceResultPage : AppCompatActivity() {
 
         databaseReference = FirebaseDatabase.getInstance().getReference("FirebaseDatabase")
 
+        //Driver Names
         val spinnerDriver1: Spinner = findViewById(R.id.P1Driver_ARR)
         val spinnerDriver2: Spinner = findViewById(R.id.P2Driver_ARR)
         val spinnerDriver3: Spinner = findViewById(R.id.P3Driver_ARR)
@@ -31,6 +37,18 @@ class AddRaceResultPage : AppCompatActivity() {
         val spinnerDriver8: Spinner = findViewById(R.id.P8Driver_ARR)
         val spinnerDriver9: Spinner = findViewById(R.id.P9Driver_ARR)
         val spinnerDriver10: Spinner = findViewById(R.id.P10Driver_ARR)
+
+        //Team names
+        val spinnerTeam1: Spinner = findViewById(R.id.P1Const_ARR)
+        val spinnerTeam2: Spinner = findViewById(R.id.P2Const_ARR)
+        val spinnerTeam3: Spinner = findViewById(R.id.P3Const_ARR)
+        val spinnerTeam4: Spinner = findViewById(R.id.P4Const_ARR)
+        val spinnerTeam5: Spinner = findViewById(R.id.P5Const_ARR)
+        val spinnerTeam6: Spinner = findViewById(R.id.P6Const_ARR)
+        val spinnerTeam7: Spinner = findViewById(R.id.P7Const_ARR)
+        val spinnerTeam8: Spinner = findViewById(R.id.P8Const_ARR)
+        val spinnerTeam9: Spinner = findViewById(R.id.P9Const_ARR)
+        val spinnerTeam10: Spinner = findViewById(R.id.P10Const_ARR)
 
         val data1 = listOf("Max Verstappen",
             "Sergio Perez",
@@ -43,7 +61,26 @@ class AddRaceResultPage : AppCompatActivity() {
             "Fernando Alonso",
             "Lance Stroll")
 
+        var driverList : ArrayList<String> = arrayListOf("Max Verstappen",
+            "Sergio Perez",
+            "Charles Leclerc",
+            "Carlos Sainz",
+            "Lewis Hamilton",
+            "George Russell",
+            "Lando Norris",
+            "Oscar Piastri",
+            "Fernando Alonso",
+            "Lance Stroll")
+
+
+        val data2 = listOf("Red Bull", "Ferrari", "AMG Mercedes", "McLaren", "Aston Martin")
+
+        var teamList : ArrayList<String> = arrayListOf("Red Bull", "Ferrari", "AMG Mercedes", "McLaren", "Aston Martin")
+
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, data1)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapter2 = ArrayAdapter(this, android.R.layout.simple_spinner_item, data2)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinnerDriver1.adapter = adapter
@@ -57,15 +94,47 @@ class AddRaceResultPage : AppCompatActivity() {
         spinnerDriver9.adapter = adapter
         spinnerDriver10.adapter = adapter
 
+        spinnerTeam1.adapter = adapter2
+        spinnerTeam2.adapter = adapter2
+        spinnerTeam3.adapter = adapter2
+        spinnerTeam4.adapter = adapter2
+        spinnerTeam5.adapter = adapter2
+        spinnerTeam6.adapter = adapter2
+        spinnerTeam7.adapter = adapter2
+        spinnerTeam8.adapter = adapter2
+        spinnerTeam9.adapter = adapter2
+        spinnerTeam10.adapter = adapter2
+
+        var raceYear : EditText = findViewById(R.id.YearAdd)
+        var raceDate : EditText = findViewById(R.id.DateAdd)
+        var raceLaps : EditText = findViewById(R.id.LapAdd)
+        var raceLoc : EditText = findViewById(R.id.LocationAdd)
 
         addRace.setOnClickListener {
 
-//            var raceDetails = Race_Results()
-//            var usersReference = databaseReference.child("Race Results")
-//            var newRecordReference = usersReference.push()
-//            newRecordReference.setValue(raceDetails)
-//            var dataKey = newRecordReference.key
+            var raceDetails = Race_Results(raceYear.text.toString().toInt(),
+                                            raceDate.text.toString(),
+                                            raceLoc.text.toString(),
+                                            raceLaps.text.toString().toInt(),
+                                            driverList,
+                                            teamList)
 
+            databaseReference.child("Race Results").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot.children
+                    var childCount = 0
+                    for (i in snapshot.children) {
+                        childCount++
+                    }
+                    snapshot.child("Race Number $childCount").ref.setValue(raceDetails)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+                    Toast.makeText(this, "Success - ADD", Toast.LENGTH_SHORT).show()
         }
 
         backBtn.setOnClickListener {
